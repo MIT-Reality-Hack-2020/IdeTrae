2e7d5aba5f9f6220ac2b26c7cb09dc8db9de66f0
mohrtw <mohrtw@gmail.com>
2020-01-19 11:00:21 -0800

    submit

---
+++ Assets/BeginIntroNarration.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginIntroNarration : MonoBehaviour
+{
+	bool canFire;
+	float timer;
+	private AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canFire = true;
+        //timer = time.time;
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canFire) Invoke("Sound", 2);
+    }
+    void Sound(){
+    	sound.Play(0);
+    	canFire = false;
+    }
+}
