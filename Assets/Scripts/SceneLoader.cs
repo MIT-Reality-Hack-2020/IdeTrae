62965e60403c4b145a2ab12cf4c3e4a1a84be041
mohrtw <mohrtw@gmail.com>
2020-01-19 09:50:49 -0800

    final sounds

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,23 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 
-    
+
 {
     private Vector3 orig_pos;
     public bool ideation_room;
-    public GameObject confetti;
+    private static GameObject confetti;
     public GameObject grabSoundObj;
-    public GameObject telePortSoundObj; 
+    public GameObject telePortSoundObj;
+    public GameObject confettiSoundObj;
+    private ParticleSystem confetti_particle;
 
     AudioSource grabSound;
     AudioSource telePortSound;
+    AudioSource confettiSound;
 
     private void Start()
     {
         ideation_room = false;
-        confetti.SetActive(false);
         telePortSound = telePortSoundObj.GetComponent<AudioSource>();
         grabSound = grabSoundObj.GetComponent<AudioSource>();
+        orig_pos = GetComponent<Collider>().gameObject.transform.position;
+        confettiSound = confettiSoundObj.GetComponent<AudioSource>();
+
+        if (confetti == null)
+        {
+            confetti = GameObject.Find("Confetti");
+            confetti.SetActive(false);
+        }
     }
 
     void OnTriggerEnter(Collider collider)
@@ -45,23 +55,23 @@ public class SceneLoader : MonoBehaviour
         {
             //if (ideation_room)
             //{
-                telePortSound.Play(0);
+            telePortSound.Play(0);
 
-                Debug.Log("return collider");
-                SceneManager.LoadScene("Scene 1 - Main Hall");
-                Debug.Log("Teleporting to Normcore");
-                ideation_room = false;
+            Debug.Log("return collider");
+            SceneManager.LoadScene("Scene 1 - Main Hall");
+            Debug.Log("Teleporting to Normcore");
+            ideation_room = false;
             //}
-            
+
         }
-        
+
         else if (collider.gameObject.tag == "Button")
         {
             grabSound.Play(0);
 
 
             Vector3 pos = collider.gameObject.transform.position;
-            orig_pos = pos;
+
             if (pos.y >= -5.51)
             {
                 pos.y -= 0.005f;
@@ -69,8 +79,10 @@ public class SceneLoader : MonoBehaviour
             collider.gameObject.transform.position = pos;
             Debug.Log("asdkoadskijoasijdoi");
             confetti.SetActive(true);
+            confettiSound.Play(0);
+
         }
-        
+
     }
 
     private void OnTriggerExit(Collider collider)
@@ -78,7 +90,7 @@ public class SceneLoader : MonoBehaviour
         if (collider.gameObject.tag == "Button")
         {
             grabSound.Play(0);
-
+            confetti.SetActive(true);
             collider.gameObject.transform.position = orig_pos;
             Debug.Log("asdkoadskijoasijdoi");
         }
